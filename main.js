22142ed41535752586871df180163259dbc28fba
francescodelmonaco <francescodelmonaco1999@gmail.com>
2025-04-29 17:39:43 +0200

    add: snack 6

--- main.js
+++ main.js
@@ -33,14 +33,28 @@ console.log(eseguiOperazione(6, 2, divisione));
 
 // snack 4
 function creaTimer(ms) {
-    setTimeout(() => console.log("Tempo scaduto!"), ms)
+    return setTimeout(() => console.log("Tempo scaduto!"), ms)
 };
 
 creaTimer(2000);
 
 // snack 5
 function stampaOgniSecondo(text) {
-    setInterval(() => console.log(text), 1000)
+    return setInterval(() => console.log(text), 1000)
 };
 
 stampaOgniSecondo("Stampo ogni secondo...");
+
+// snack 6
+function creaContatoreAutomatico(ms) {
+    let counter = 0;
+
+    return () => {
+        setInterval(() => {
+            console.log(counter++);
+        }, ms);
+    };
+}
+
+const avviaContatore = creaContatoreAutomatico(1000);
+avviaContatore();
