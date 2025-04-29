91c8410b769c08d671f25f1bb3dee0202e19dc68
francescodelmonaco <francescodelmonaco1999@gmail.com>
2025-04-29 17:06:32 +0200

    add: snack 1

---
+++ main.js
@@ -0,0 +1,18 @@
+// snack 1
+function somma(a, b) {
+    return a + b;
+};
+
+console.log(somma(5, 10));
+
+// funzione anonima
+const funzioneSommaAnonima = function (a, b) {
+    return a + b;
+};
+
+console.log(funzioneSommaAnonima(10, 22));
+
+// arrow
+const funzioneSommaArrow = (a, b) => a + b;
+
+console.log(funzioneSommaArrow(2, 7));
